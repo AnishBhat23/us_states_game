87a80ffcf903285c3780d997bd0138ed740464bf
AnishBhat23 <emsonanish12@gmail.com>
2025-04-07 20:54:00 +0530

    US States Game Update 3

--- main.py
+++ main.py
@@ -13,17 +13,17 @@ scores = Scoreboard()
 
 
 states_data = pandas.read_csv("50_states.csv")
-guessed_states = []
+all_states = states_data.state.to_list()
 
 while scores.score < 50:
     answer_state = screen.textinput(title=f"{scores.score}/50 States guessed", prompt="What's another state's name?\n").title()
     if answer_state == "Exit":
-        #states_to_learn = pandas.DataFrame(~(states_data.isin([guessed_states])))
-        #states_to_learn.to_csv("states_to_learn.csv")
+        all_states_data = pandas.DataFrame(all_states)
+        all_states_data.to_csv("states_to_learn.csv")
         break
-    if states_data.isin([answer_state]).any().any():
+    if answer_state in all_states:
         # print(states_data[states_data.state == str(answer_state)].x)
-        guessed_states.append(answer_state)
-        scores.display_score(int(states_data[states_data.state == str(answer_state)].x.iloc[0]), int(states_data[states_data.state == str(answer_state)].y.iloc[0]), answer_state)
+        all_states.remove(answer_state)
+        scores.display_score(int(states_data[all_states == str(answer_state)].x.iloc[0]), int(states_data[all_states == str(answer_state)].y.iloc[0]), answer_state)
         scores.increase_score()
         screen.update()
